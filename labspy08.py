dfcdc64039a4f39218261508e76a0e4b58f2b726
andiniimaulidaa <andinimaulida1306@gmail.com>
2025-01-14 19:27:48 +0700

    Add files via upload

---
+++ labspy08.py
@@ -0,0 +1,13 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct 7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)) on win32
+
+Type "help", "copyright", "credits" or "license()" for more information.
+
+RESTART: C:/Users/ASUS/Downloads/labspy08.py ==========
+
+Masukkan nama lengkap: Andin maulida
+
+Masukkan nomor telepon: 0895379858148
+
+Masukkan email: andinimaulida1306@gmail.com
+
+Data pendaftaran valid.
